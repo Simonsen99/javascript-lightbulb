9b5b8f8eca556c02be6f45f4bbd3a8cee6aa6af2
Simonsen99 <eaa23chsim@students.eaaa.dk>
2024-09-01 16:58:53 +0200

    Completed the task

---
+++ javascript-lightbulb-main/js/script.js
@@ -0,0 +1,9 @@
+function onBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/on.jpg";
+  }
+  
+  function offBulb() {
+    const bulb = document.getElementById("bulb");
+    bulb.src = "img/off.jpg";
+  }
